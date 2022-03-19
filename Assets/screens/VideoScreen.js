306a6f94e88f7e379ccc931356bfde2ca92ea2ee
Hanzla <hanzlamunir4269@gmail.com>
2022-03-19 19:47:00 +0500

    commit

--- Assets/screens/VideoScreen.js
+++ Assets/screens/VideoScreen.js
@@ -1,12 +1,11 @@
 import {Text, View} from 'react-native';
 import React, {Component} from 'react';
-import Video from 'react-native-video';
 
 export default class VideoScreen extends Component {
   render() {
     return (
       <View>
-        <Video source={require('../../Assets/abc.mp4')} />
+        <Text>VideoScreen</Text>
       </View>
     );
   }
