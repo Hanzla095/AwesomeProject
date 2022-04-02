6f62ab7fa665877bb83605ca588f83929cec24bd
Hanzla <hanzlamunir4269@gmail.com>
2022-04-02 15:55:16 +0500

    commit

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import {Text, View} from 'react-native';
-import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import React from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text>App</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
 }
